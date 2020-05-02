a78c9b8e648903eedbcc34ffbe0b41ee16c636fa
hasitha <hasitha.abeynayake@gmail.com>
2020-05-02 18:21:02 +1000

    Basic project structure completed with hardcoded data

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProjectList from './ProjectList';
+
+const ProjectSummary = () => {
+	return (
+		<div className="card z-depth-0 project-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">Project Title</span>
+				<p>Posted by Hasitha Abeynayake</p>
+				<p className="grey-text">02nd May, 3 p.m.</p>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectSummary;
