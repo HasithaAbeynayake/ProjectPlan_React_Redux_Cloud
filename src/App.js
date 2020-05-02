2f1c94e059279a4dbc4c982921fd4325a5947997
hasitha <hasitha.abeynayake@gmail.com>
2020-05-02 15:24:13 +1000

    Navigation panel created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
 	render() {
-		return <div className="App"></div>;
+		return (
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+				</div>
+			</BrowserRouter>
+		);
 	}
 }
 
