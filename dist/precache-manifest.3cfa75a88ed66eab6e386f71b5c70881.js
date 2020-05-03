8b45d318a24423b49090a41b1c11153776b22cb0
hasitha <hasitha.abeynayake@gmail.com>
2020-05-04 09:14:35 +1000

    Notification panel created and application hosted in firebase cloud

---
+++ dist/precache-manifest.3cfa75a88ed66eab6e386f71b5c70881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e4633f09e83c7c578b756101669569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1afc119b0cbab8cbf054",
+    "url": "/static/css/main.72a048f1.chunk.css"
+  },
+  {
+    "revision": "d91ff61aa18415b0e689",
+    "url": "/static/js/2.29881d34.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.29881d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afc119b0cbab8cbf054",
+    "url": "/static/js/main.d6df67d6.chunk.js"
+  },
+  {
+    "revision": "c1271e88ade9f7110ed2",
+    "url": "/static/js/runtime-main.aa35bfc7.js"
+  }
+]);
